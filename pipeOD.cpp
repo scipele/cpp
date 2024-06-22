be49f2db502c0c42a64ab65d02a43f13d36a4c59
scipele <152800577+scipele@users.noreply.github.com>
2024-06-22 12:27:44 -0500

    Update pipeOD.cpp

--- pipeOD.cpp
+++ pipeOD.cpp
@@ -1,46 +1,24 @@
-// Looping with For and with an array
 #include <iostream>
+#include <map>
 
-double getPipeOD(double nps) {
-    // Array defines the pipe NPS and cooresponding od for pipe sizes 0.5" thru 12"
-    double const pipeSzOD[13][2] = {
-        {0.5,  0.84 },   // NPS 0.5" has OD 0.84
-        {0.75, 1.05 },
-        {1,    1.315},
-        {1.25, 1.66 },
-        {1.5,  1.9  },
-        {2,    2.375},
-        {2.5,  2.875},
-        {3,    3.5  },
-        {4,    4.5  },
-        {6,    6.625},
-        {8,    8.625},
-        {10,   10.75}, 
-        {12,   12.75}   
-    };
-        
-    if (nps > 12) {
-        return nps;  // For Pipe Sizes Larger than 12" the OD is equal to the nom pipe size (nps)
-    } else {
-        for (int i = 0; i<=12; i++) {   // loop thru array elements until finding a match 
-            if (pipeSzOD[i][0] == nps ) {
-                return pipeSzOD[i][1];
-                break;
-            }
-        } 
-    }
-    // If the input doesn't match any known pipe size, return 0 value
-    return 0.0; // Default value
-}
+double getPipeOD(double nps);
 
 int main()
 {
-    // message for user
     std::cout << "Enter Nom Pipe Size :";       
-    double input;                               // declare 'input' as type double
-    std::cin >> input;                          // get user input using the standard cin function
-    double od = getPipeOD(input);               // declare 'od' as return from the getPipeOD Custom Function  
-    std::cout << "Pipe OD = " << od << "\n";    // output OD to screen
+    double nps;  std::cin >> nps; 
+    
+    //call function getPipeOD to return a double with the pipe outside diameter
+    std::cout << "Pipe OD = " << getPipeOD(nps) << "\n";    // output OD to screen
 
     return 0;
+}
+
+double getPipeOD(double nps) {
+    std::map<double, double> pipOdMap = { {0.5,0.84}, {0.75,1.05}, {1,1.315}, {1.25,1.66}, {1.5,1.9}, {2,2.375}, {2.5,2.875}, {3,3.5}, {4,4.5}, {6,6.625}, {8,8.625}, {10,10.75}, {12,12.75} };
+        if(nps > 12.0) {
+                return nps;       // For Pipe Sizes Larger than 12" the OD is equal to the nom pipe size (nps)
+        } else {
+            return pipOdMap[nps];  // Otherwise return the pipe OD from the Map related pair 
+        }
 }
