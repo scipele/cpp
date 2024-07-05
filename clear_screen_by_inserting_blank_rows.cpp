45ed384042b1aab738621532894b73b37531a9fe
scipele <152800577+scipele@users.noreply.github.com>
2024-07-05 10:27:47 -0500

    Add files via upload

---
+++ clear_screen_by_inserting_blank_rows.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << std::string(30, '\n');    // clear screen
+    std::cout << std::endl;
+    
+    return 0;
+}
