da1387f675f4ecfaee8ba46f8bba920238aec769
scipele <152800577+scipele@users.noreply.github.com>
2024-07-19 09:53:33 -0500

    Update conv_ft_in.cpp

--- piping/conv_ft_in.cpp
+++ piping/conv_ft_in.cpp
@@ -1,1 +1,29 @@
-//test
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+
+	std::string inp = "32'-7 5/16";
+
+	std::vector<std::string> parsed;
+	std::string tmp;
+
+	bool is_prev_num = false;
+
+	for ( auto c : inp ) {
+
+		if is_prev_num == isdigit(c) {
+			if ( c != " " || c != "" ) { 
+				tmp += c;
+			}
+		} else {
+			parsed.push_back(tmp);
+			std::cout "\n" << tmp;
+			tmp = c;  // starts new tmp
+		}
+	}
+	
+	return 0;
+
+}
