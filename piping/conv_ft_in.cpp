c4fe7a0d09cac21f7e89807e11095e1c663aed6a
scipele <152800577+scipele@users.noreply.github.com>
2024-07-19 22:07:53 -0500

    Update conv_ft_in.cpp

--- piping/conv_ft_in.cpp
+++ piping/conv_ft_in.cpp
@@ -4,26 +4,89 @@
 
 int main() {
 
-	std::string inp = "32'-7 5/16";
+	std::string inp = " 32'-7 5/16";
 
 	std::vector<std::string> parsed;
 	std::string tmp;
-
-	bool is_prev_num = false;
-
-	for ( auto c : inp ) {
-
-		if is_prev_num == isdigit(c) {
-			if ( c != " " || c != "" ) { 
-				tmp += c;
-			}
+    
+    // set starting condition
+    bool is_prev_num = false;
+    if (isdigit(inp[0]) && inp[0] != ' ') {
+         is_prev_num = true;
+    }
+	
+	auto itt = inp.begin();
+	char c;
+	
+	while ( itt != inp.end() ) {
+	    c = *itt;
+		if (isdigit(c) == is_prev_num) {
+		    if( c != ' ' && c != '-') {
+		        tmp += c;
+		        // if on last iteration then force push back
+		        if ( itt == inp.end() - 1) {
+		            parsed.push_back(tmp);
+		        }
+		    }
 		} else {
-			parsed.push_back(tmp);
-			std::cout "\n" << tmp;
-			tmp = c;  // starts new tmp
+			is_prev_num = false;
+			if( c != ' ' && c != '-') {
+			    parsed.push_back(tmp);
+			    tmp = c;  // starts new tmp
+			}
 		}
+		if (isdigit(c)) is_prev_num = true;
+		itt++;
+		
 	}
 	
-	return 0;
+	for ( auto p : parsed) {
+	    std::cout << p << "\n";
+	}
+}#include <string>
+# <iostream>
+#include <string>
+#include <vector>
 
+int main() {
+
+	std::string inp = " 32'-7 5/16";
+
+	std::vector<std::string> parsed;
+	std::string tmp;
+    
+    // set starting condition
+    bool is_prev_num = false;
+    if (isdigit(inp[0]) && inp[0] != ' ') {
+         is_prev_num = true;
+    }
+	
+	auto itt = inp.begin();
+	char c;
+	
+	while ( itt != inp.end() ) {
+	    c = *itt;
+		if (isdigit(c) == is_prev_num) {
+		    if( c != ' ' && c != '-') {
+		        tmp += c;
+		        // if on last iteration then force push back
+		        if ( itt == inp.end() - 1) {
+		            parsed.push_back(tmp);
+		        }
+		    }
+		} else {
+			is_prev_num = false;
+			if( c != ' ' && c != '-') {
+			    parsed.push_back(tmp);
+			    tmp = c;  // starts new tmp
+			}
+		}
+		if (isdigit(c)) is_prev_num = true;
+		itt++;
+		
+	}
+	
+	for ( auto p : parsed) {
+	    std::cout << p << "\n";
+	}
 }
