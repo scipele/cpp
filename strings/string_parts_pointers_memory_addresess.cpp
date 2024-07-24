91ecbf73e3e3239eb72fc63a885f32209d5ca7c7
Tony <152800577+scipele@users.noreply.github.com>
2024-07-24 10:20:49 -0500

    Update string_parts_pointers_memory_addresess.cpp

--- strings/string_parts_pointers_memory_addresess.cpp
+++ strings/string_parts_pointers_memory_addresess.cpp
@@ -5,28 +5,38 @@
 // Also the example shows how pointers can be defined to different parts of a string
 
 int main() {
-    std::string str = "Hello, World!";
+    std::string str = "Show Memory Address of Each Character";
 
     // Pointer to the beginning of the string
     char *ptr_begin = &str[0];
 
-    // Pointer to the 7th character (index 6) in the string
-    char *ptr_middle = &str[6];
+    // Pointer to the 5th character (index 4) in the string
+    char *ptr_space = &str[4];
 
     // Pointer to the end of the string
     char *ptr_end = &str[str.length() - 1];
 
-    std::cout << "Pointer to the beginning (ptr_begin): " << static_cast<void*>(ptr_begin) << " (" << *ptr_begin << ")\n";
-    std::cout << "Pointer to the middle (ptr_middle)  : " << static_cast<void*>(ptr_middle) << " (" << *ptr_middle << ")\n";
-    std::cout << "Pointer to the end (ptr_end)        : " << static_cast<void*>(ptr_end) << " (" << *ptr_end << ")\n";
+    for (int i=0; i<50; i++) std::cout << "\n"; // print several line feeds to clear out screen
+    std::cout << std::endl;
 
-    std::cout << "First part of the string:  " << std::string(ptr_begin, ptr_middle - ptr_begin) << std::endl;
-    std::cout << "Second part of the string: " << std::string(ptr_middle, ptr_end - ptr_middle + 1) << std::endl;
+    std::cout << "Example to Show how a string is stored in c++:";
+    std::cout << "\t with string input -> " << str << "\n\n";
+
+
+    std::cout << "Pointer to the beginning (ptr_begin): " << static_cast<void*>(ptr_begin) << " (" << *ptr_begin << ")\n";
+    std::cout << "Pointer to the middle (ptr_space)   : " << static_cast<void*>(ptr_space) << " (" << *ptr_space << ")\n";
+    std::cout << "Pointer to the end (ptr_end)        : " << static_cast<void*>(ptr_end) << " (" << *ptr_end << ")\n\n";
+
+    std::cout << "First part of the string:  " << std::string(ptr_begin, ptr_space - ptr_begin ) << "\n";
+    std::cout << "Second part of the string: " << std::string(ptr_space, ptr_end - ptr_space ) << "\n\n";
 
     // Next loop thru each character and print the memory address of each character
+    std::cout << "Indx  Ascii   Char    Hexidecimal Address of Memmory Address where data is stored" << std::string(ptr_space, ptr_end - ptr_space ) << std::endl;
     for (int i = 0; i < str.length(); ++i) {
         std::cout << "[" << i << "]\t" << static_cast<int>(str[i]) << "\t'" << str[i] << "'\t" << static_cast<void*>(&str[i]) << std::endl;
     }
+
+    std::cout << std::endl;
 
     return 0;
 }
