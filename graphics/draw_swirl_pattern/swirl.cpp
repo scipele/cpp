5887e7e7eae109063145afaef840139347ba563e
Tony <152800577+scipele@users.noreply.github.com>
2024-07-26 22:41:37 -0500

    Add files via upload

--- graphics/draw_swirl_pattern/swirl.cpp
+++ graphics/draw_swirl_pattern/swirl.cpp
@@ -1,37 +1,45 @@
 #include <Windows.h>
 #include <sstream>
 #include <cmath>
-#include <stdlib.h>
+
+struct point {
+    double x;
+    double y;
+};
 
 // Forward declaration of the window procedure
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 
 // Entry point for Windows GUI applications
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
+    
     // Register the window class
     const wchar_t CLASS_NAME[] = L"MyWindowClass";
+    
     WNDCLASSW wc = {};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    wc.lpfnWndProc = WindowProc;           // lpfnWndProc is a pointer to an application-defined function called the window procedure or window proc.
+    wc.hInstance = hInstance;           // hInstance is the handle to the application instance. Get this value from the hInstance parameter of wWinMain
+    wc.lpszClassName = CLASS_NAME;      // lpszClassName is a string that identifies the window class.
     RegisterClassW(&wc);
 
     // Create the window
     HWND hWnd = CreateWindowExW(
         0,                              // Optional window styles
         CLASS_NAME,                     // Window class
-        L"My Window",                   // Window title
+        L"Swirl Patter in Window",      // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
 
         // Size and position
         // orig code  -->         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        // CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         CW_USEDEFAULT, CW_USEDEFAULT, 1920, 1080,
+
         NULL,       // Parent window
         NULL,       // Menu
         hInstance,  // Instance handle
         NULL        // Additional application data
-    );
+        );
+    
     if (hWnd == NULL) {
         return 0;
     }
@@ -49,15 +57,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
     return 0;
 }
 
-
 void line(HDC& hdc, double x1, double y1, double x2, double y2) {
-    MoveToEx(hdc, x1, y1, NULL);
-    LineTo(hdc, x2, y2);
-}
+    long x1_lg = static_cast<long>(x1); 
+    long y1_lg = static_cast<long>(y1); 
+    long x2_lg = static_cast<long>(x2); 
+    long y2_lg = static_cast<long>(y2); 
 
-void chg_color(HDC& hdc, HPEN& hOldPen, int r, int g, int b) {
-    HPEN hPen = CreatePen(PS_SOLID, 1, RGB(r, g, b));
-    hOldPen = (HPEN)SelectObject(hdc, hPen);
+    MoveToEx(hdc, x1_lg, y1_lg, NULL);
+    LineTo(hdc, x2_lg, y2_lg);
 }
 
 void drawFilledTriangle(HDC hdc, std::initializer_list<POINT> vertices, COLORREF color) {
@@ -75,24 +82,18 @@ void drawFilledTriangle(HDC hdc, std::initializer_list<POINT> vertices, COLORREF
     DeleteObject(hBrush);
 }
 
-void show_variable_value(HDC hdc, std::string str, double dbl_val, double loc_x, double loc_y) {
-    // Draw the calculated value as text
-    std::stringstream ss;
-    ss << str << dbl_val;
-    std::string narrowText = ss.str();
-    int wstr_size = MultiByteToWideChar(CP_UTF8, 0, narrowText.c_str(), -1, NULL, 0);
-    std::wstring wideText(wstr_size, 0);
-    MultiByteToWideChar(CP_UTF8, 0, narrowText.c_str(), -1, &wideText[0], wstr_size);
-    TextOutW(hdc, loc_x, loc_y, wideText.c_str(), wideText.length());
+// function to convert double to long
+long dtl(double num) {
+    return static_cast<long>(num);
 }
 
-
 // Window procedure
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
+
         case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
@@ -101,7 +102,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             HBRUSH hBrush, hOldBrush;
 
             //using screen size of 1920 x 1080
-            // Calculations
             // define border coordinates            
             const double xmin = 450;
             const double ymin = 10;
@@ -117,71 +117,69 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             double xcen = (xmax - xmin) /2 ;
             double ycen = (ymax - ymin) /2 ;
 
-            double x1, y1, x2, y2, x3, y3, x4, y4, x1_prev, y1_prev, x2_prev, y2_prev, x3_prev, y3_prev, x4_prev, y4_prev, xd_prev, yd_prev, len_lg_prev, len_lg, len_sh, a1, b2 ;
-            double ang = 0.15, ang_delta = 0.15;      // radians
+            // Declare Variables
+            point p[10];
+            p[0] = { 450, 10 };
+            p[1] = { 1430, 10 };
+            p[2] = { 1430, 990 };
+            p[3] = { 450, 990 };
 
-            // second layer (starting near 0,0 - corner)
-            //start x1,y1, x2,y2
-            x1_prev = 450;
-            y1_prev = 10;
-            x2_prev = 1430;
-            y2_prev = 10;
-            x3_prev = 1430;
-            y3_prev = 990;
-            x4_prev = 450;
-            y4_prev = 990;
-
+            double len_p, len, a1, b, c, cx, cy, k, xd, yd;
+            double ang = 0.1, ang_delta = 0.1;      // radians
+            len_p = p[1].x - p[0].x;    // starting length since its at zero degree angle at start
+            k = tan(ang_delta);
 
             // Start Loop Here Later
-            for (int i = 1; i < 3; i++) {
+            for (int i = 1; i < 50; i++) {
 
-                // Compute Previous Line Length
-                xd_prev = x2_prev - x1_prev;
-                yd_prev = y2_prev - y1_prev;
-                len_lg_prev = sqrt(pow(xd, 2) + pow(yd, 2));
+                // Solve the new length of the triangle with multiple formulas solution
+                a1 = len_p / (1 + pow(k, 2));
+                b = k * a1;                         // opposite side
+                c = sqrt( pow(a1, 2) + pow(b, 2));  // New length of triangle hypotenuse
+                
+                // now compute the x/y delta coordinates
+                cx = c * cos(ang);                  // Longer Side offset
+                cy = c * sin(ang);                  // Shorter Side Offset
 
-                len_lg = len_prev / ( 1 + pow(tan(ang), 2) );      // triangle long side new length
-                len_sh = tan(ang) * len_lg;                             // short side offset
+                p[4].x = p[0].x + cx;
+                p[4].y = p[0].y + cy;
+                line(hdc, p[0].x, p[0].y, p[4].x, p[4].y);
 
-                x2 = x1_prev + a1;
-                y2 = y1_prev + b2;
-                line(hdc, x1_prev, y1_prev, x2, y2);
+                p[5].x = p[1].x - cy;
+                p[5].y = p[1].y + cx;
+                line(hdc, p[1].x, p[1].y, p[5].x, p[5].y);
 
-                x3 = x2_prev - b2;
-                y3 = y2_prev + a1;
-                line(hdc, x2_prev, y2_prev, x3, y3);
+                p[6].x = p[2].x - cx;
+                p[6].y = p[2].y - cy;
+                line(hdc, p[2].x, p[2].y, p[6].x, p[6].y);
 
-                x4 = x3_prev - a1;
-                y4 = y3_prev - b2;
-                line(hdc, x3_prev, y3_prev, x4, y4);
-
-                x1 = x4_prev + b2;
-                y1 = y4_prev - a1;
-                line(hdc, x4_prev, y4_prev, x1, y1);
+                p[7].x = p[3].x + cy;
+                p[7].y = p[3].y - cx;
+                line(hdc, p[3].x, p[3].y, p[7].x, p[7].y);
 
                 // draw filled in triangle for odd iterations
-                if (i % 2 == 1) drawFilledTriangle(hdc, { {x1_prev, y1_prev}, {x2_prev, y2_prev}, {x2, y2} }, RGB(0, 255, 0));
-        
+                if (i % 2 == 1) drawFilledTriangle(hdc, { { dtl(p[0].x), dtl(p[0].y) }, { dtl(p[1].x), dtl(p[1].y) }, { dtl(p[4].x), dtl(p[4].y) } }, RGB(234, 63, 247));
+                if (i % 2 == 1) drawFilledTriangle(hdc, { { dtl(p[1].x), dtl(p[1].y) }, { dtl(p[5].x), dtl(p[5].y) }, { dtl(p[2].x), dtl(p[2].y) } }, RGB(0, 255, 0));
+                if (i % 2 == 1) drawFilledTriangle(hdc, { { dtl(p[2].x), dtl(p[2].y) }, { dtl(p[3].x), dtl(p[3].y) }, { dtl(p[6].x), dtl(p[6].y) } }, RGB(234, 63, 247));
+                if (i % 2 == 1) drawFilledTriangle(hdc, { { dtl(p[3].x), dtl(p[3].y) }, { dtl(p[7].x), dtl(p[7].y) }, { dtl(p[0].x), dtl(p[0].y) } }, RGB(0, 255, 0));
+
+                // Compute starting length for next iteration
+                xd = p[4].x - p[7].x;
+                yd = p[4].y - p[7].y;
+                len_p = sqrt( pow(xd, 2) + pow(yd, 2));  
+
                 // Set previous values for next iteration                
                 ang += ang_delta;
-                x1_prev = x1;
-                y1_prev = y1;
-                x2_prev = x2;
-                y2_prev = y2;
-                x3_prev = x3;
-                y3_prev = y3;
-                x4_prev = x4;
-                y4_prev = y4;
-
+                p[0].x = p[7].x;   p[0].y = p[7].y;
+                p[1].x = p[4].x;   p[1].y = p[4].y;
+                p[2].x = p[5].x;   p[2].y = p[5].y;
+                p[3].x = p[6].x;   p[3].y = p[6].y;
             }
 
-            //print values on screen
-            show_variable_value(hdc, "c =", c, 10, 10);
-
-  
             EndPaint(hWnd, &ps);
-            return 0;
         }
+        return 0;
+
     }
     return DefWindowProc(hWnd, message, wParam, lParam);
 }
