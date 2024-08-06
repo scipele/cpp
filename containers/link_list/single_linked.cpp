bcb01c59fcc25fd8853d7b6b80a9604f88ded368
Tony Sciple <scipele@yahoo.com>
2024-08-06 13:57:25 -0500

    Update single_linked.cpp

--- containers/link_list/single_linked.cpp
+++ containers/link_list/single_linked.cpp
@@ -1,5 +1,12 @@
 // filename:  linked_list.cpp
-
+// this is an example of how to implement a linked list class in c++
+// 
+// this simple example only stores an integer value and a pointer tonthe next node,
+// however note that the struct could have other variables added as needed
+//
+// 
+// by T. Sciple,  8/6/2024
+//
 #include <iostream>
 
 class Node {
@@ -31,6 +38,7 @@ void InsertAtFront(Node** head, int new_value) {
 
 
 void InsertAtBack(Node** head, int new_value) {        	// 1. Prepare new Node
+	// 1.  Prepare for addition of a new node
 	Node* new_node = new Node();
 	new_node->value = new_value;
 	new_node->next = NULL;
@@ -46,6 +54,21 @@ void InsertAtBack(Node** head, int new_value) {        	// 1. Prepare new Node
 		}
 	// 4. set the value of the last node
 	chk_last->next = new_node;
+}
+
+
+void insertAfter(Node* prev_node, int new_value) {
+     // 1. Make sure prev node is not NULL
+    if (prev_val == NULL) {
+	std::cout << "Previous value cant be NULL";
+	return;
+     }
+     // 2.  Prepare for addition of new node
+     Node* new_node = new Node();
+     new_node->value = new_value;
+     // 3.  insert new node after previou value referenced 
+     new_node->next = prev_node->next;
+     prev_node->next = new_node;
 }
 
 
@@ -66,6 +89,7 @@ int main () {
 	InsertAtFront(&head, 5);
 	InsertAtFront(&head, 3);
 	InsertAtBack(&head, 35);
+	InsertAfter(second, 25);
 	print_list(head);
 return 0;
 }
