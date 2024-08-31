c9fc082e74f90429786cc4defb9adac1989ca488
Tony Sciple <scipele@yahoo.com>
2024-08-31 11:05:50 -0500

    chgd to class implementation

--- oop/pipeData/PipeData.hpp
+++ oop/pipeData/PipeData.hpp
@@ -1,9 +1,14 @@
+// Filename:    PipeData.hpp
+//
+// Purpose:     This is the class header file that defines all the outline of the class
+
+//  By:         T.Sciple, 8/31/2024
+
 #pragma once
-#ifndef PIPEDATA_HPP
+#ifndef PIPEDATA_HPP  // <- if "PIPEDATA_HPP" is not defined then the following define will be executed, otherwise compiler will skip to the #endif // PIPEDATA_HPP
 #define PIPEDATA_HPP
 
 #include <string>
-
 
 class PipeData
 {
@@ -17,17 +22,25 @@ private:
     double emptyWtPerFt = 0;
     double waterWtPerFt = 0;
     double pipeWtFullPerFt= 0;
-    int sizeIndex = 0;
-    int schIndex = 0;
 
     // Function Prototypes for class member functions
-    std::string getSize();
-    std::string getSched();
+    std::string get_size();
+    std::string get_str_from_user(std::string user_msg);
+    std::string get_sched();
     std::string toUpperCase(const std::string str);
+    int get_od_and_thk();
+    void print_any_errors(int result);
+    void get_addn_data();
+
     
 public:
     // Constructor Prototype
     PipeData();
+    
+    // Public Member Functions or methods
+    void GetUserInput();
+    void PrintPipeData();
+
     //Destructor Prototype
     ~PipeData();
 };
--- oop/pipeData/main.cpp
+++ oop/pipeData/main.cpp
@@ -1,9 +1,46 @@
+// Filename:    main.cpp
+//
+// Purpose:     This is an example usage of the PipeData Class
+//              - See header file 'PipeData.hpp' for basic outline of the class
+//              - See the implementation file 'PipeData.cpp' for Public and Private Member Function Details
+//
+// Dependencies:    Header/Implementation Files             Class Name
+//                  PipeData.hpp                            PipeData
+//                  PipeData.cpp                            PipeData
+//
+
+//                This directive must be used in the tasks.json file so that the compiler will compile all the files in the current path
+//                      "*.cpp",  // Compile all .cpp files in the current directory
+//                      comment this out -> // "${file}",  which tells the compiler to compile only the current file
+//
+//  By:         T.Sciple, 8/31/2024
+
+
 #include <iostream>
-#include "PipeData.hpp" // Include the header file for GetPipeData
+#include "PipeData.hpp" // Header file definition for PipeData Class, note that the implementation file is PipeData.cpp which is also
 
 int main(int argc, char const *argv[])
 {
-    PipeData pd;
+    std::string loopAgain;
+    do
+    {
+        // Instantiate an Object PipeData
+        PipeData pd;
+
+        // Call Member Function / Method  which will get the asize and schedule from the user
+        pd.GetUserInput();
+
+        // Call the following Member Function / Method which will print the detailed data
+        pd.PrintPipeData();
+
+        //Keep looping or 'q' to quit
+        std::cout << "Enter to Continue, q to quit ";
+        std::getline(std::cin, loopAgain);
+
+    } while (loopAgain != "q");
+
+    //pause for user input to prevent window from closing
+    system("pause");
 
     return 0;
 }
