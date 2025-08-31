e0caf733b866428708d49f6fd13d9cedd15433b0
scipele <scipele@yahoo.com>
2025-08-30 23:10:52 -0500

    update with dark background

--- graphics/draw_swirl_pattern/swirl.cpp
+++ graphics/draw_swirl_pattern/swirl.cpp
@@ -1,10 +1,16 @@
+//| Item	     | Main Program Documentation Notes                            |
+//|--------------|-------------------------------------------------------------|
+//| Filename     | swirl.cpp                                                   |
+//| EntryPoint   | wWinMain                                                    |
+//| Purpose      | create triangles                                            |
+//| Inputs       | hard coded                                                  |
+//| Outputs      | graphic window with filled triangles                        |
+//| Dependencies | standard only                                               |
+//| By Name,Date | T.Sciple, 8/30/2025                                         |
+
 // This program starts by drawing an square and then given an angle in radians triangle i solved for squares
 // inside the other squares that progressively get smaller the program loops to recalculate the triangle for each of
-// the 50 iterations.  For odd iterations the triangles are drawn filled in with two different RGB colors
-//
-// Using command line compiler in Windows 11, g++ -o swirl.exe swirl.cpp -mwindows
-//
-// by.  T. Sciple 7/27/2024 
+// the various number of iterations.  For odd iterations the triangles are drawn filled in with two different RGB colors
 
 #include <Windows.h>
 #include <sstream>
@@ -19,7 +25,6 @@ struct point {
 // Forward declaration of the window procedure
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-
 // Entry point for Windows GUI applications
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
     
@@ -27,7 +32,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
     const wchar_t CLASS_NAME[] = L"MyWindowClass";
     
     WNDCLASSW wc = {};
-    wc.lpfnWndProc = WindowProc;           // lpfnWndProc is a pointer to an application-defined function called the window procedure or window proc.
+    wc.lpfnWndProc = WindowProc;        // lpfnWndProc is a pointer to an application-defined function called the window procedure or window proc.
     wc.hInstance = hInstance;           // hInstance is the handle to the application instance. Get this value from the hInstance parameter of wWinMain
     wc.lpszClassName = CLASS_NAME;      // lpszClassName is a string that identifies the window class.
     RegisterClassW(&wc);
@@ -62,9 +67,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return 0;
 }
+
 
 void line(HDC& hdc, double x1, double y1, double x2, double y2) {
     // Cast double values to long to avoid warnings
@@ -77,14 +82,13 @@ void line(HDC& hdc, double x1, double y1, double x2, double y2) {
     LineTo(hdc, x2_lg, y2_lg);
 }
 
-void drawFilledTriangle(HDC hdc, double x1, double y1, double x2, double y2, double x3, double y3, COLORREF color) {
 
+void drawFilledTriangle(HDC hdc, double x1, double y1, double x2, double y2, double x3, double y3, COLORREF color) {
     // Convert points to POINT structure with static_cast
     POINT pts[3];
     pts[0] = { static_cast<LONG>(x1), static_cast<LONG>(y1) };
     pts[1] = { static_cast<LONG>(x2), static_cast<LONG>(y2) };
     pts[2] = { static_cast<LONG>(x3), static_cast<LONG>(y3) };
-    
 
     HBRUSH hBrush = CreateSolidBrush(color); // Create a brush with the specified color
     HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrush);
@@ -92,6 +96,7 @@ void drawFilledTriangle(HDC hdc, double x1, double y1, double x2, double y2, dou
     SelectObject(hdc, hOldBrush);
     DeleteObject(hBrush);
 }
+
 
 // Window procedure
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -105,7 +110,17 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
             HDC hdc = BeginPaint(hWnd, &ps);
 
             HPEN hPen, hOldPen;
-            HBRUSH hBrush, hOldBrush;
+            //HBRUSH hBrush, hOldBrush;
+
+            HBRUSH hBrush = CreateSolidBrush(RGB(18, 18, 18));
+            
+            // Fill the entire client area with a dark background
+            RECT rect;
+            GetClientRect(hWnd, &rect);
+            FillRect(hdc, &rect, hBrush);
+            // Clean up
+            DeleteObject(hBrush);
+            //EndPaint(hWnd, &ps);            
 
             //using screen size of 1920 x 1080
             // define border coordinates            
@@ -131,12 +146,12 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
             p[3] = { 450, 990 };
 
             double len_p, len, a1, b, c, cx, cy, k, xd, yd;
-            double ang = 0.10, ang_delta = 0.10;      // radians
+            double ang = 0.05, ang_delta = 0.05;      // radians
             len_p = p[1].x - p[0].x;    // starting length since its at zero degree angle at start
             k = tan(ang_delta);
 
             // Start Loop Here Later
-            for (int i = 1; i < 60; i++) {
+            for (int i = 1; i < 100; i++) {
 
                 // Solve the new length of the triangle with multiple formulas solution
                 a1 = len_p / (1 + pow(k, 2));
@@ -164,11 +179,11 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                 line(hdc, p[3].x, p[3].y, p[7].x, p[7].y);
 
                 // draw filled in triangle for odd iterations
-                if (i % 2 == 1) drawFilledTriangle(hdc, p[0].x, p[0].y, p[1].x, p[1].y, p[4].x, p[4].y, RGB(150, 118, 230));
-                if (i % 2 == 1) drawFilledTriangle(hdc, p[1].x, p[1].y, p[5].x, p[5].y, p[2].x, p[2].y, RGB(241, 15, 230));
-                if (i % 2 == 1) drawFilledTriangle(hdc, p[2].x, p[2].y, p[3].x, p[3].y, p[6].x, p[6].y, RGB(150, 118, 230));
-                if (i % 2 == 1) drawFilledTriangle(hdc, p[3].x, p[3].y, p[7].x, p[7].y, p[0].x, p[0].y, RGB(241, 15, 230));
-               
+                if (i % 2 == 1) drawFilledTriangle(hdc, p[0].x, p[0].y, p[1].x, p[1].y, p[4].x, p[4].y, RGB(255, 255, 0));
+                if (i % 2 == 1) drawFilledTriangle(hdc, p[1].x, p[1].y, p[5].x, p[5].y, p[2].x, p[2].y, RGB(0, 204, 255));
+                if (i % 2 == 1) drawFilledTriangle(hdc, p[2].x, p[2].y, p[3].x, p[3].y, p[6].x, p[6].y, RGB(187, 134, 252));
+                if (i % 2 == 1) drawFilledTriangle(hdc, p[3].x, p[3].y, p[7].x, p[7].y, p[0].x, p[0].y, RGB(153, 255, 153));
+                
                 // Compute starting length for next iteration
                 xd = p[4].x - p[7].x;
                 yd = p[4].y - p[7].y;
@@ -181,11 +196,9 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
                 p[2].x = p[5].x;   p[2].y = p[5].y;
                 p[3].x = p[6].x;   p[3].y = p[6].y;
             }
-
             EndPaint(hWnd, &ps);
         }
         return 0;
-
     }
     return DefWindowProc(hWnd, message, wParam, lParam);
 }
--- piping/pipe_OD.cpp
+++ piping/pipe_OD.cpp
@@ -16,7 +16,21 @@ int main()
 }
 
 double getPipeOD(double nps) {
-    std::map<double, double> pipOdMap = { {0.5,0.84}, {0.75,1.05}, {1,1.315}, {1.25,1.66}, {1.5,1.9}, {2,2.375}, {2.5,2.875}, {3,3.5}, {4,4.5}, {6,6.625}, {8,8.625}, {10,10.75}, {12,12.75} };
+    std::map<double, double> pipOdMap = { {0.5,0.84},
+                                          {0.75,1.05},
+                                          {1,1.315},
+                                          {1.25,1.66},
+                                          {1.5,1.9},
+                                          {2,2.375},
+                                          {2.5,2.875},
+                                          {3,3.5},
+                                          {4,4.5},
+                                          {5,5.625},
+                                          {6,6.625},
+                                          {8,8.625},
+                                          {10,10.75},
+                                          {12,12.75}
+                                        };
         if(nps > 12.0) {
                 return nps;       // For Pipe Sizes Larger than 12" the OD is equal to the nom pipe size (nps)
         } else {
