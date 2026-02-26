6cb96cbb6457138ceb98a0f75533a14945484d4c
scipele <scipele@yahoo.com>
2026-02-25 23:01:15 -0600

    update

--- misc/num_conversions/eight_bit_int.cpp
+++ misc/num_conversions/eight_bit_int.cpp
@@ -13,6 +13,8 @@
 #include <cstdint>
 #include <bitset>
 #include <iomanip>
+#include <cmath>
+#include <string>
 
 int main()
 {
@@ -22,11 +24,37 @@ int main()
     // 1 000  0000  The leftmost bit is the sign bit, which indicates whether the number is positive (0) or negative (1).
     // The remaining 7 bits represent the magnitude of the number.
 
-    for (int n = -128; n <= 127; ++n) {
-        std::cout   
-                    << std::setw(5) << std::left << static_cast<int>(static_cast<int8_t>(n)) 
-                    << "binary: " << std::bitset<8>(static_cast<int8_t>(n)) << std::endl;
+    //for (int n = -128; n <= 127; ++n) {
+    //    std::cout   
+    //                << std::setw(5) << std::left << static_cast<int>(static_cast<int8_t>(n)) 
+    //                << "binary: " << std::bitset<8>(static_cast<int8_t>(n)) << std::endl;
+    //}
+    
+    // illustrate how to convert 8 bit binary to decimal
+    std::cout << "\nConverting 8-bit binary to decimal:\n";
+    std::string binaryStr = "10111"; // This represents -128 in 8-bit two's complement
+
+    int len = binaryStr.length(); // Get the index of the last character
+    
+
+    for (int i = 0; i < len; i++) {
+        std::cout << "(" << binaryStr[i] << " x 2^" << (len - 1 - i) << ")";
+        if (i < len - 1) std::cout << " + ";
     }
+    std::cout << "\n";
+    int n;
+    int sum = 0;
+
+    for (int i = 0; i < len; i++) {
+        n = binaryStr[i] - '0';
+        int exponent = len - 1 - i; // Calculate the exponent based on the position of the bit
+        int cur_num = n * pow(2, exponent);
+        std::cout << "(" << cur_num << ")";
+        if (i < len - 1) std::cout << " + ";
+        sum += cur_num; // Convert char to int and calculate value
+    }
+    std::cout << " = " << sum << std::endl;
+
 
     return 0;
 }
