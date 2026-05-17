4e1e67f3b5d9a277274f78e42fecb72a7f853976
scipele <scipele@yahoo.com>
2026-05-17 16:11:10 -0500

    blanks

--- misc/hello_world/hello_world.cpp
+++ misc/hello_world/hello_world.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+
+void print_blanks() {
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << "\n";
+    }
+}
+
+
 int main()
 {
-    std::cout << "hello world!";
+    print_blanks();
+    std::cout << "Hello world!\n";
+    print_blanks();
+  
+    std::cout << std::endl;
+    
     return 0;
 }
